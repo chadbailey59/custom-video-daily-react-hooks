0eeed1b8007d24e3e63d0b72bbe44fd3579be6fc
Chad Bailey <chadbailey@gmail.com>
2023-10-19 16:14:12 -0500

    querying for audio elements

--- src/components/Call/Call.js
+++ src/components/Call/Call.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState, useCallback, useMemo, useRef, useEffect } from 'react';
 import {
   useParticipantIds,
   useScreenShare,
@@ -14,7 +14,7 @@ import UserMediaError from '../UserMediaError/UserMediaError';
 export default function Call() {
   /* If a participant runs into a getUserMedia() error, we need to warn them. */
   const [getUserMediaError, setGetUserMediaError] = useState(false);
-
+  const audioRef = useRef(null);
   /* We can use the useDailyEvent() hook to listen for daily-js events. Here's a full list
    * of all events: https://docs.daily.co/reference/daily-js/events */
   useDailyEvent(
@@ -35,16 +35,18 @@ export default function Call() {
     [remoteParticipantIds, screens],
   );
 
+  useEffect(() => {
+    console.log('audioref current: ', audioRef.current);
+    if (audioRef.current) {
+      const audioTags = audioRef.current.getAllAudio();
+      console.log({ audioTags });
+    }
+  }, [remoteParticipantIds]);
+
   const renderCallScreen = () => (
     <div className={screens.length > 0 ? 'is-screenshare' : 'call'}>
       {/* Your self view */}
-      {localParticipant && (
-        <Tile
-          id={localParticipant.session_id}
-          isLocal
-          isAlone={isAlone}
-        />
-      )}
+      {localParticipant && <Tile id={localParticipant.session_id} isLocal isAlone={isAlone} />}
       {/* Videos of remote participants and screen shares */}
       {remoteParticipantIds?.length > 0 || screens?.length > 0 ? (
         <>
@@ -54,7 +56,7 @@ export default function Call() {
           {screens.map((screen) => (
             <Tile key={screen.screenId} id={screen.session_id} isScreenShare />
           ))}
-          <DailyAudio />
+          <DailyAudio ref={audioRef} />
         </>
       ) : (
         // When there are no remote participants or screen shares
